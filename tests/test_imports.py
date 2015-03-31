af13375322f9e5eeee41de31b7e2bca45ad334ef
srwareham <srwareham@hotmail.com>
2015-03-30 20:44:33 -0400

    Added tests package with context

---
+++ tests/test_imports.py
@@ -0,0 +1,6 @@
+__author__ = 'srwareham'
+from .context import campaignadvisor
+
+if __name__ == "__main__":
+    print campaignadvisor.resources.PROJECT_ROOT_PATH
+    print campaignadvisor.archiver.__author__
