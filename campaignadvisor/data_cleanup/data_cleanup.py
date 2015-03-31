2c0854354c9b6d541693ae02f784dbdb09fa6245
srwareham <srwareham@hotmail.com>
2015-03-30 20:42:32 -0400

    Update data_cleanup

--- campaignadvisor/data_cleanup/data_cleanup.py
+++ campaignadvisor/data_cleanup/data_cleanup.py
@@ -1,15 +1,13 @@
-import os
 import pandas as pd
 import csv
 
-# Directory path constants
-ROOT_PATH = os.path.abspath(__file__ + "/../../")
-DATA_DIR_PATH = os.path.join(ROOT_PATH, "data_files")
+import campaignadvisor.resources
 
-# File path constants
-CONTRIBUTIONS_PATH = os.path.join(DATA_DIR_PATH, "contributions.csv")
-ZIP_CODES_PATH = os.path.join(DATA_DIR_PATH, "zip_code_database.csv")
-COUNTY_VOTES_PATH = os.path.join(DATA_DIR_PATH, "county_votes.csv")
+"""
+Data cleanup utility
+
+May eventually be used to store cleaned tables instead.
+"""
 
 # Zip code file-specific constants
 # Replacement text for when there is no county name
@@ -30,30 +28,36 @@ CONTRIBUTIONS_FIELD_NAMES = ["cmte_id", "cand_id", "cand_nm", "contbr_nm", "cont
                              "contbr_employer", "contbr_occupation", "contb_receipt_amt", "contb_receipt_dt",
                              "receipt_desc", "memo_cd", "memo_text", "form_tp", "file_num", "tran_id", "election_tp"]
 
-# Mapping of zipcode to county name
-zip_codes = {}
 
-with open(ZIP_CODES_PATH, 'rb') as csvfile:
-    reader = csv.DictReader(csvfile, ZIP_CODES_FIELD_NAMES)
-    for row in reader:
-        # Look out for types, they're different everywhere and need to be consistent
-        zipcode = str(row['zip'])
-        # Change in implementation may require state being passed as well
-        # state = row['state']
-        county = str(row['county'])
-        # Cannot split no length strings
-        if len(county) == 0:
-            continue
-        split = county.split()
-        # TODO: will cause errors when city is valid.
-        # TODO: add city whitelist, perhaps don't use it at all
-        # Throw out stop words at the end of the county name
-        if split[-1] in ZIP_STOP_WORDS:
-            county = " ".join(split[:-1]).strip()
-        zip_codes[zipcode] = county
+def get_zip_codes_map():
+    # Mapping of zipcode to county name
+    zip_codes = {}
+    zip_codes_resource = campaignadvisor.resources.get_resource("zip_code_database.csv")
+    with zip_codes_resource.get_file() as csvfile:
+        reader = csv.DictReader(csvfile, ZIP_CODES_FIELD_NAMES)
+        for row in reader:
+            # Look out for types, they're different everywhere and need to be consistent
+            zipcode = str(row['zip'])
+            # Change in implementation may require state being passed as well
+            # state = row['state']
+            county = str(row['county'])
+            # Cannot split no length strings
+            if len(county) == 0:
+                continue
+            split = county.split()
+            # TODO: will cause errors when city is valid.
+            # TODO: add city whitelist, perhaps don't use it at all
+            # Throw out stop words at the end of the county name
+            if split[-1] in ZIP_STOP_WORDS:
+                county = " ".join(split[:-1]).strip()
+            zip_codes[zipcode] = county
+    return zip_codes
 
 
-def get_county(zip_code):
+# TODO: find way that does not take keyword arguments. Makes it look optional when it is not
+def get_county(zip_code, zip_codes_map=None):
+    if zip_codes_map is None:
+        raise
     try:
         zip_code = str(int(zip_code))
     except:
@@ -62,8 +66,8 @@ def get_county(zip_code):
     # Convert long zip codes to short
     if len(zip_code) == LONG_ZIP_CODE_LENGTH:
         zip_code = zip_code[:SHORT_ZIP_CODE_LENGTH]
-    if zip_code in zip_codes:
-        return zip_codes[zip_code]
+    if zip_code in zip_codes_map:
+        return zip_codes_map[zip_code]
     else:
         return "%s" % NO_COUNTY_NAME
 
@@ -71,5 +75,13 @@ def get_county(zip_code):
 def example_zip_to_county():
     # Rename CONTRIBUTIONS_PATH if you would like to use a smaller test file
     # CONTRIBUTIONS_PATH = os.path.join(DATA_DIR_PATH, "VA.csv")
-    contributions = pd.read_csv(CONTRIBUTIONS_PATH)
-    print contributions["contbr_zip"].apply(get_county)
+    contributions_resource = campaignadvisor.resources.get_resource("contributions.csv")
+    contributions_resource.download_if_necessary()
+    contributions = pd.read_csv(contributions_resource.get_local_path())
+    zip_codes_map = get_zip_codes_map()
+    # NOTE: keyword argument is not optional. The apply method is just a little hanky
+    # Full blown code smell, probably a better way.
+    print contributions["contbr_zip"].apply(get_county, zip_codes_map=zip_codes_map)
+
+if __name__ == "__main__":
+    example_zip_to_county()
