d365a8cdd3135e0e1f2582409246cab033f3b7e7
srwareham <srwareham@hotmail.com>
2015-04-02 01:05:10 -0400

    Modified context files to allow for running from any working directory

--- campaignadvisor/data_cleanup/context.py
+++ campaignadvisor/data_cleanup/context.py
@@ -1,6 +1,10 @@
 __author__ = 'srwareham'
 import sys
 import os
-PROJ_PATH = os.path.abspath('.')
+
+THIS_DIR = os.path.dirname(os.path.abspath(__file__))
+PARENT_DIR = os.path.dirname(THIS_DIR)
+GRANDPARENT_DIR = os.path.dirname(PARENT_DIR)
+PROJ_PATH = GRANDPARENT_DIR
 sys.path.insert(0, PROJ_PATH)
 import campaignadvisor
--- examples/context.py
+++ examples/context.py
@@ -1,6 +1,9 @@
 __author__ = 'srwareham'
 import sys
 import os
-PROJ_PATH = os.path.abspath('.')
+
+THIS_DIR = os.path.dirname(os.path.abspath(__file__))
+PARENT_DIR = os.path.dirname(THIS_DIR)
+PROJ_PATH = PARENT_DIR
 sys.path.insert(0, PROJ_PATH)
 import campaignadvisor
