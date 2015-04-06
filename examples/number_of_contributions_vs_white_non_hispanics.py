90816c80961d5742a08bb3dfcc95624e22130f15
Dianwen Li <dianwen.li@duke.edu>
2015-04-05 22:33:21 -0400

    Added example correlations and plots

--- examples/number_of_contributions_vs_white_non_hispanics.py
+++ examples/number_of_contributions_vs_white_non_hispanics.py
@@ -19,30 +19,20 @@ if __name__ == "__main__":
     # Get job data
     rural_atlas_data_10_resource = campaignadvisor.resources.get_resource("RuralAtlasData10.xls")
     workbook = pd.ExcelFile(rural_atlas_data_10_resource.get_local_path())
-    jobs = workbook.parse(u"Jobs")
+    jobs = workbook.parse(u"People")
 
     # Get contributions and aggregate them by state_county
     contributions_resource = campaignadvisor.resources.get_resource("contributions.csv")
     contributions_dataframe = pd.read_csv(contributions_resource.get_local_path())
     # TODO: perhaps throw out counties that have less than 'n' contributions
     contributions_dataframe['FIPS'] = contributions_dataframe['contbr_zip'].apply(zip_to_fips)
-
-    # Create new table where each row contains a county's median contribution amount, and its job statistics
-    county_state_group_contrib = contributions_dataframe.groupby('FIPS')[['FIPS', 'contb_receipt_amt']].median()
-
-    # Join only those with intersection
-    joined = pd.merge(county_state_group_contrib, jobs, on="FIPS", sort=False, how="inner")
-
     # Count the number of contributions in each county
-    joined['ContributionsCount'] = contributions_dataframe.groupby('FIPS').size()
-
+    jobs['ContributionsCount'] = contributions_dataframe.groupby('FIPS').size()
     #Exclude count from FIPS code 0
-    joined = joined[joined['FIPS'] != 0]
+    jobs = jobs[jobs['FIPS'] != 0]
 
-    # Filter out counties with less than 100 contributions
-    joined['ContributionsCount'] = contributions_dataframe.groupby('FIPS').size()
-    joined = joined[joined['ContributionsCount'] >= 100]
-    
+    jobs['ContributionsCount'] = contributions_dataframe.groupby('FIPS').size()
+    jobs = jobs[jobs['ContributionsCount'] >= 100]
     # Plot scatter plot
-    joined.plot(kind='scatter', x='UnempRate2012', y='contb_receipt_amt')
+    jobs.plot(kind='scatter', x='WhiteNonHispanicPct2010', y='ContributionsCount')
     pylot.show()
