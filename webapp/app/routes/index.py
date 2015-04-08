76fc3e12714784274143044c6b7bc46afd625cfd
Yu Zhou Lee <lee.yuzhou@gmail.com>
2015-04-08 17:27:50 -0400

    Created interactable US map.

--- webapp/app/routes/index.py
+++ webapp/app/routes/index.py
@@ -6,6 +6,10 @@ import json
 def root():
 	return app.send_static_file('index.html')
 
-@app.route('/getCountyLines', methods=['GET'])
+@app.route('/api/getCountyLines', methods=['GET'])
 def getCountyLines():
-	return app.send_static_file('mapdata/us-counties.json')
+	return app.send_static_file('mapdata/county.json')
+	
+@app.route('/api/getStateLines', methods=['GET'])
+def getStateLines():
+	return app.send_static_file('mapdata/states.json')
--- webapp/app/static/js/app.js
+++ webapp/app/static/js/app.js
@@ -1,18 +1,24 @@
 // Declare app level module which depends on filters, and services
-angular.module('CampaignAdvisor', ['ngResource', 'ngRoute', 'ui.bootstrap', 'ui.date'])
+angular.module('CampaignAdvisor', ['ngResource', 'ngRoute', 'ui.bootstrap', 'ui.date', 'ngMaterial'])
   .config(['$routeProvider', function ($routeProvider) {
     $routeProvider
       .when('/', {
         templateUrl: 'views/home/home.html', 
         controller: 'HomeController',
         resolve: {
-    		countyLines: ['$http', function ($http){
-    			return $http.get('/api/getCountyLines').then(
-                  function success(response) { return response; },
-                  function error(reason)     { return reason; }
-                );
-    		}]
-    	}
+      		countyLines: ['$http', function ($http){
+      			return $http.get('/api/getCountyLines').then(
+                    function success(response) { return response.data; },
+                    function error(reason)     { return reason; }
+                  );
+      		}],
+          stateLines: ['$http', function ($http){
+            return $http.get('/api/getStateLines').then(
+                    function success(response) { return response.data; },
+                    function error(reason)     { return reason; }
+                  );
+          }]
+    	  }
     })
     .otherwise({redirectTo: '/'});
   }]);
