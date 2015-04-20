1d22c28edb78a1f680580af1e3515cfc9814d853
srwareham <srwareham@hotmail.com>
2015-04-20 00:47:44 -0400

    Cleaned up normalize

--- campaignadvisor/data_cleanup/normalize.py
+++ campaignadvisor/data_cleanup/normalize.py
@@ -1,105 +1,95 @@
 import pandas as pd
 import numpy as np
-import csv
-from context import campaignadvisor
 from sklearn import preprocessing
 
 
 class Normalizer():
-	MIN_VAL = 0
-	MAX_VAL = 1
+    MIN_VAL = 0
+    MAX_VAL = 1
 
-	"""
-	input: Series
-	output: Series
-	"""
+    """
+    input: Series
+    output: Series
+    """
 
-	def __init__(self, df, alg):
-		self.df = df
-		self.alg = alg
-		# algorithm types =
-		# 	'minmax' is rescaling,
-		# 	'std' is standardization,
-		#	'unit' is scaling to unit length
+    def __init__(self, df, alg="minmax"):
+        self.df = df
+        self.alg = alg
 
-	def __repr__(self):
-		return self.df
+    # algorithm types =
+    # 'minmax' is rescaling,
+    # 	'std' is standardization,
+    #	'unit' is scaling to unit length
 
-	def normalize(self, feature):
-		# check if column min and max equals the global min and max
-		if self.MAX_VAL != self.df[feature].max() and self.MIN_VAL != self.df[feature].min():
-			return True
-		if self.alg == 'minmax':
-			normalize_minmax(self, feature)
-		elif self.alg == 'std':
-			normalize_std(self, feature)
+    def __repr__(self):
+        return self.df
 
-	def normalize_minmax(self, feature):
-		self.MAX_VAL = self.df[feature].max()
-		self.MIN_VAL = self.df[feature].min()
-		feature_minmax = feature + '_zscore'
-		self.df[feature_minmax] = (self.df[feature] - self.MIN_VAL) / (self.MAX_VAL - self.MIN_VAL)
-		# return self.df[feature_minmax]
+    def normalize_minmax(self, feature):
+        self.MAX_VAL = self.df[feature].max()
+        self.MIN_VAL = self.df[feature].min()
+        feature_minmax = feature + '_minmax'
+        self.df[feature_minmax] = (self.df[feature] - self.MIN_VAL) / (self.MAX_VAL - self.MIN_VAL)
 
-	def normalize_std(self, feature):
-		feature_zscore = feature + '_zscore'
-		self.df[featire_zscore] = (self.df[feature] - self.df[feature].mean())/self.df[feature].std(ddof=0)
-		# return self.df[feature_zscore]
+    # return self.df[feature_minmax]
+
+    def normalize_std(self, feature):
+        feature_zscore = feature + '_zscore'
+        self.df[feature_zscore] = (self.df[feature] - self.df[feature].mean()) / self.df[feature].std(ddof=0)
+
+    # return self.df[feature_zscore]
+
+    def normalize(self, feature):
+        # check if column min and max equals the global min and max
+        if self.MAX_VAL == self.df[feature].max() and self.MIN_VAL == self.df[feature].min():
+            return True
+        if self.alg == 'minmax':
+            self.normalize_minmax(feature)
+        elif self.alg == 'std':
+            self.normalize_std(feature)
+
 
 def main():
-	"""
-	jobs_name = campaignadvisor.dataframe_holder.JOBS
+    """
+    jobs_name = campaignadvisor.dataframe_holder.JOBS
     votes_name = campaignadvisor.dataframe_holder.VOTES
     jobs = campaignadvisor.dataframe_holder.get_dataframe(jobs_name)
     votes = campaignadvisor.dataframe_holder.get_dataframe(votes_name)
     votes['clean_fips'] = votes['fips_code']
 
-	# country statistics dataframe
+    # country statistics dataframe
     df = pd.merge(votes, jobs, on='clean_fips', sort=False, how="inner")
-	"""
+    """
 
-	# example dataframe
-	df = pd.DataFrame({'a': [2, 4, 5], 'b': [3, 9, 4]}, dtype=np.float)
-	print(df)
-	
-	# get feature list
-	features_to_scale = list(df.columns)
-	for feature in df.columns:
-		if feature == 'clean_fps' or feature == 'fips_code':
-			features_to_scale.remove(feature)
-	
-	# instantiate Normalizer object with given algorithm
-	df_scaled = Normalizer(df, alg='minmax')
+    # example dataframe
+    df = pd.DataFrame({'a': [2, 4, 5], 'b': [3, 9, 4]}, dtype=np.float)
+    print(df)
 
-	# normalize only features to be scaled
-	for feature in features_to_scale:
-		df_scaled.normalize(feature)
+    # get feature list
+    features_to_scale = list(df.columns)
+    for feature in df.columns:
+        if feature == 'clean_fps' or feature == 'fips_code':
+            features_to_scale.remove(feature)
 
-	# testing
-	print df_scaled.df
-	print test_normalize(df, features_to_scale, alg='minmax')
+    # instantiate Normalizer object with given algorithm
+    df_scaled = Normalizer(df, alg='minmax')
+
+    # normalize only features to be scaled
+    for feature in features_to_scale:
+        df_scaled.normalize(feature)
+
+    # testing
+    print df_scaled.df
+    print test_normalize(df, features_to_scale, alg='minmax')
+
 
 def test_normalize(df, features, alg):
-	if alg == 'minmax':
-		scale = preprocessing.MinMaxScaler().fit(df[features])
-	elif self.alg == 'std':
-		scale = preprocessing.StandardScaler().fit(df[features])
-	df_scaled = scale.transform(df[features])
-	return df_scaled
+    if alg == 'minmax':
+        scale = preprocessing.MinMaxScaler().fit(df[features])
+    elif alg == 'std':
+        scale = preprocessing.StandardScaler().fit(df[features])
+    df_scaled = scale.transform(df[features])
+    return df_scaled
+
 
 if __name__ == "__main__":
     main()
-
-"""
-County statistics dataframe (normalize)
-
-example:
-	def normalize(col, algorithm='lin'):
-		input: series
-		output: series
-
-ignore clean_fps column
-# round function to approximate to closest tenths value
-
-Normalization algorithm (linear, logarithmic,) --> expressed as a parameter
-"""
