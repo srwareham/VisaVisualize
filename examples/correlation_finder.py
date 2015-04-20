eb23ceaa5da20afdae9590ecd41788b561e97b32
srwareham <srwareham@hotmail.com>
2015-04-20 01:48:34 -0400

    Added correlation finder

--- examples/correlation_finder.py
+++ examples/correlation_finder.py
@@ -3,11 +3,6 @@ import pandas as pd
 import matplotlib.pyplot as pyplot
 
 
-def plot_columns(df, x_column, y_column):
-    df.plot(kind='scatter', x=x_column, y=y_column, logx=True)
-    pyplot.show()
-
-
 def get_correlated_fields(df, field_name, ignored_fields=None, ignore_field_name=True):
     if ignored_fields is None:
         ignored_fields = []
@@ -46,11 +41,23 @@ def main():
     county_statistics_name = campaignadvisor.dataframe_holder.COUNTY_STATISTICS
     county_statistics = campaignadvisor.dataframe_holder.get_dataframe(county_statistics_name)
     county_statistics.index = county_statistics['clean_fips']
-    county_statistics['contributions_per_capita'] = county_statistics['contributions_count'] * 1.0 / county_statistics['TotalPopEst2012']
 
+    # Variable to test
+    dependent_variable = "contributions_per_capita"
 
-    dependent_variable = "clean_contribution_median"
-    print_correlated_fields(county_statistics, dependent_variable, ignored_fields=['clean_contribution_mean'])
+    # After look at printed results, can determine what we would like the independent variable to be and can plot
+    independent_variable = "Ed4CollegePlusPct0812"
+
+    # Plot findings with log scale on y-axis
+    #county_statistics.plot(kind='scatter', x=independent_variable, y=dependent_variable, logy=True)
+    #pyplot.show()
+
+    # Will fail if have not pulled and deleted county_statistics.pik;  Put in workaround for the interim
+    try:
+        print_correlated_fields(county_statistics, dependent_variable, ignored_fields=['clean_contribution_mean'])
+    except KeyError:
+        county_statistics['contributions_per_capita'] = county_statistics['contributions_count'] * 1.0 / county_statistics['TotalPopEst2012']
+        print_correlated_fields(county_statistics, dependent_variable, ignored_fields=['clean_contribution_mean'])
 
 if __name__ == "__main__":
     main()
